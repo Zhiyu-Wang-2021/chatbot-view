0d8c6a73d73798e21e58c00609a6a77312bcaff5
Zhiyu Wang <zcabanq@ucl.ac.uk>
2023-03-02 14:55:57 +0000

    fix: changed the base url public ip of django on vm

--- src/env.js
+++ src/env.js
@@ -1,3 +1,3 @@
 export const iamKey = "fK1ry7X3D0HFVHC_kJXKlLA6900sxeHH6Yc0MKtEdGHm"
 export const instanceId = "d049a552-ee69-4a04-aa9b-1fa4e7994ece"
-export const backendBaseUrl = "http://10.0.0.4:8000" // "http://127.0.0.1:8000/"//"http://51.132.145.57:8000/" // "http://127.0.0.1:8000/"
+export const backendBaseUrl = "http://51.132.145.57:8000/" // "http://127.0.0.1:8000/"//"http://51.132.145.57:8000/" // "http://127.0.0.1:8000/"
